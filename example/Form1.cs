6c424770f057bcff85dd13d84551b424b0b107c4
ilchoTaleski <ile_taleskipr@hotmail.com>
2018-06-10 02:50:43 +0200

    another attempt

---
+++ example/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FruityMatch
+{
+    public partial class Form1 : Form
+    {
+        Image playButton;
+        public Form1()
+        {
+            InitializeComponent();
+            playButton = Properties.Resources.play;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImageUnscaled(playButton, 0, 0);
+        }
+    }
+}
